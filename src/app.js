94d623993fef70ec6e50cab3eb395a79bb39357e
FelipeMaxRosa <felipemax.suporteti@gmail.com>
2020-07-06 22:52:06 -0300

    Ajuste script de start

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import accountsRouter from "../routes/accountsRouter.js";
 //require("dotenv").config();
 
 // Banco no Atlas
-// const URL = `mongodb+srv://${process.env.USERDB}:${process.env.PASSDB}@api-test-iiinl.mongodb.net/igti-my-bank?retryWrites=true&w=majority`;
-const URL = `mongodb+srv://admin:1q2w3e@api-test-iiinl.mongodb.net/igti-my-bank?retryWrites=true&w=majority`;
+const URL = `mongodb+srv://${process.env.USERDB}:${process.env.PASSDB}@api-test-iiinl.mongodb.net/igti-my-bank?retryWrites=true&w=majority`;
+// const URL = `mongodb+srv://admin:1q2w3e@api-test-iiinl.mongodb.net/igti-my-bank?retryWrites=true&w=majority`;
 
 // Conectando ao Banco
 const connectDB = async () => {
@@ -26,4 +26,4 @@ const app = express();
 app.use(express.json());
 app.use("/account", accountsRouter);
 
-app.listen(3000, connectDB);
+app.listen(process.env.PORT, connectDB);
